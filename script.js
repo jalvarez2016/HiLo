178b026413ed7e975f4959de5870e7957c4819b3
Jason <alvarezcoraiza@citypolyhs.org>
2017-02-15 21:21:52 +0000

    < similar to another project>

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+var secretNum = 7;
+   $("button").click(function(){
+      var guess =$("#pass").val(); 
+      if(secretNum > guess){
+        $("#hint").html("That guess is too low, try again ");
+      }
+      else if(secretNum < guess){
+        $("#hint").html("That guess is too high, try again ");
+      }
+      else if (secretNum==guess){
+      $("#hint").html("WOOOOOOOHHHH!!!!!! The secret number is " + secretNum);
+      }
+      else {
+        $("#hint").html("Please type a number and that number should between 1 ~ 10");
+      }
+  });
+});
